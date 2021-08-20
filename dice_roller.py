421173fab62218452c97138a5623731d5f2d57b7
lowrykun <jason@finch.com>
2021-08-20 08:10:49 -0600

    Multiple Dice with Inputs

--- dice_roller.py
+++ dice_roller.py
@@ -1,13 +1,19 @@
 
 
 def main():
-    import random;
-    dice_rolls = 2
+    import random
+    dice_rolls = int(input('Number of Dice? '))
+    dice_sides = int(input('Sides on the Dice? '))
     dice_sum = 0
     for i in range(0, dice_rolls):
-        roll = random.randint(1, 6)
+        roll = random.randint(1, dice_sides)
         dice_sum += roll
-        print(f'You rolled a {roll}')
+        if roll == 1:
+            print(f'Fail: Rolled a {roll}')
+        elif roll == dice_sides:
+            print(f'Success: Rolled a {dice_sides}')
+        else:
+            print(f'You rolled a {roll}')
     print(f'The sum of the dice is {dice_sum}');
 
 
